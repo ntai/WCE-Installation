c6fd875358f4ead2eefae653344882e24bdba409
Naoyuki Tai <ntai@smartfruit.com>
2011-12-10 02:34:38 -0500

    "follow-up" is for post-installation action after the installation. The post-installation does  Add "phone home" ping script  Add MIT programming movies  via /var/www/post-installation.sh shell script. To make this to work, WCE triage CD needs to be at least v0.50 since it is the first version to support the post installation hook.

---
+++ follow-up/bin/post-installation.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+echo "Adding WCE Follow up"
+cd /mnt/disk2
+wget -q -O - http://wcesrv/wce-follow-up.tar | tar xf -
+disksize_m=$(df -m | grep /mnt/disk2 | awk '{print $2}')
+
+# Making sure that the access timestamp and computer-uuid is cleared off
+mkdir -p /mnt/disk2/var/lib/world-computer-exchange/
+rm -f /mnt/disk2/var/lib/world-computer-exchange/access-timestamp /mnt/disk2/var/lib/world-computer-exchange/computer-uuid
+
+# 
+echo -n 'http://wce.footprintllc.com/About/Ping/PingBO1.aspx' > /mnt/disk2/var/lib/world-computer-exchange/access-url
+
+if [ $disksize_m -ge 34000 ] ; then
+  echo "Adding additional contents"
+  cd /mnt/disk2/usr/local/share/wce/content_archive_v3_Oct_3_11/EN/Program
+  wget -q -O - http://wcesrv/additional-payload.tar | tar xf -
+fi
+sleep 3
+echo ""
+reboot
