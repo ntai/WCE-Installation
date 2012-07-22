7dce38727b16c946a617b817c6a9ea7ec9a42578
Naoyuki Tai <ntai@smartfruit.com>
2012-07-21 22:44:24 -0400

    Support V0.5x and V0.6x mount points.

--- follow-up/bin/post-installation.sh
+++ follow-up/bin/post-installation.sh
@@ -1,22 +1,27 @@
 #!/bin/sh
 
 echo "Adding WCE Follow up"
-cd /mnt/disk2
+if [ -d /mnt/wce_install_target ] ; then
+TARGET=/mnt/wce_install_target
+else
+TARGET=/mnt/disk2
+fi
+
+cd $TARGET
 wget -q -O - http://wcesrv/wce-follow-up.tar | tar xf -
-disksize_m=$(df -m | grep /mnt/disk2 | awk '{print $2}')
+disksize_m=$(df -m | grep $TARGET | awk '{print $2}')
 
 # Making sure that the access timestamp and computer-uuid is cleared off
-mkdir -p /mnt/disk2/var/lib/world-computer-exchange/
-rm -f /mnt/disk2/var/lib/world-computer-exchange/access-timestamp /mnt/disk2/var/lib/world-computer-exchange/computer-uuid
+mkdir -p $TARGET/var/lib/world-computer-exchange/
+rm -f $TARGET/var/lib/world-computer-exchange/access-timestamp $TARGET/var/lib/world-computer-exchange/computer-uuid
 
 # 
-echo -n 'http://wce.footprintllc.com/portals/0/PingBO1.html' > /mnt/disk2/var/lib/world-computer-exchange/access-url
+echo -n 'http://wce.footprintllc.com/portals/0/PingBO1.html' > $TARGET/var/lib/world-computer-exchange/access-url
 
-if [ $disksize_m -ge 34000 ] ; then
-  echo "Adding additional contents"
-  cd /mnt/disk2/usr/local/share/wce/content_archive_v3_Oct_3_11/EN/Program
-  wget --progress=bar -O - http://wcesrv/additional-payload.tar | tar xf -
-fi
-sleep 3
-echo ""
-reboot
+#if [ $disksize_m -ge 34000 ] ; then
+#  echo "Adding additional contents"
+#  cd $TARGET/usr/local/share/wce/content_archive_v3_Oct_3_11/EN/Program
+#  wget --progress=bar -O - http://wcesrv/wce-contents/additional-payload.tar | tar xf -
+#fi
+sync
+echo "Installation complete."
